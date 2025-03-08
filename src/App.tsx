7bba28a24e6c46bca2e7ed782dc6539509548818
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2025-03-08 16:42:20 -0500

    basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState("Hello world!");
 
   return (
-    <div className="App">
-      <div className="text-center text-2xl font-bold bg-sky-500 text-white p-4">
-        Hello world! ...
+    <div className="flex h-screen">
+      <div className="w-1/2 h-full p-4">
+        <textarea
+          className="w-full h-full border-2 border-gray-300 p-2"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div className="w-1/2 h-full text-center text-2xl font-bold bg-sky-500 text-white p-4 flex items-center justify-center">
+        {text}
       </div>
     </div>
   );
