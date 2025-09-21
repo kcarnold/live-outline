1742704121a45439d1b35e95a03d9c486814e263
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2025-09-21 15:32:56 -0400

    update icons using inkscape

---
+++ convert-icons.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+inkscape --export-width=192 --export-height=192 --export-type=png --export-filename=public/icon-192x192.png public/icon.svg
+inkscape --export-width=512 --export-height=512 --export-type=png --export-filename=public/icon-512x512.png public/icon.svg
